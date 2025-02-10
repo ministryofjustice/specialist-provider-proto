6d57525580a1eb5f4328a0663a39af40663d93e6
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2025-02-10 15:37:26 +0000

    EL-2059: If applied, this will add mock case data which can be interacted with
    - add a basic `readme.md`
    - add additional session data
    - refer to session data correctly
    - add `id` to session data
    - loop through all cases and display them
    - for loop on accepted, closed, new and search screens
    - add `id` to name links and set a cookie based on selection
    - install `cookie-parser` middleware to help get cookies to templates
    - add `cookie-parser` middleware to routes so all templates can use it
    - set the correct cookie value based on `id`
    - update templates to use `caseSelected` variable
    - make sure cookie set at start
    - add routes for posting data on form post methods
    - update financial eligibility based on user input
    - remember what form user was on when they pressed change link, by setting a cookie
    - show third party info based on 'Yes'
    - remove `caselistUrgent` and replace with `urgent`
    - conditionally show urgentReason & clientVulnerableReason

---
+++ README.md
@@ -0,0 +1,19 @@
+# Specialist Provider Prototype
+
+This repo is a prototype of the Specialist Provider functionality within Provider Hub
+
+## Documentation for developers
+
+- After cloning this repo check your node version is 20 and higher. 
+
+- Then run the following command to instal dependencies 
+
+```bash 
+npm install
+```
+
+- Use this command to run a local server
+
+```bash
+npm run dev
+```
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,112 @@ window.GOVUKPrototypeKit.documentReady(() => {
     }
   }
 })
+
+// Set cookies
+const setCookie = (name, value, days) => {
+  let expires = "";
+  if (days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = ";expires=" + date.toUTCString();
+  }
+  document.cookie = `${name}=${value}${expires};path=/;samesite=lax`;
+}
+
+// Cookies for remembering which case has been selected
+const choiceMary = document.getElementById("0")
+choiceMary?.addEventListener("click", setCookieMary);
+
+const choiceJamil = document.getElementById("1");
+choiceJamil?.addEventListener("click", setCookieJamil);
+
+const choiceMonkey = document.getElementById("2");
+choiceMonkey?.addEventListener("click", setCookieMonkey);
+
+const choiceRoronoa = document.getElementById("3");
+choiceRoronoa?.addEventListener("click", setCookieRoronoa);
+
+const choiceNami = document.getElementById("4");
+choiceNami?.addEventListener("click", setCookieNami);
+
+const choiceMaya = document.getElementById("5");
+choiceMaya?.addEventListener("click", setCookieMaya);
+
+const choiceTrystan = document.getElementById("6");
+choiceTrystan?.addEventListener("click", setCookieTrystan);
+
+
+function setCookieMary() {
+  setCookie("cookieValueForCaseSelected", "0", 30);
+};
+
+function setCookieJamil() {
+  setCookie("cookieValueForCaseSelected", "1", 30);
+};
+
+function setCookieMonkey() {
+  setCookie("cookieValueForCaseSelected", "2", 30);
+};
+
+function setCookieRoronoa() {
+  setCookie("cookieValueForCaseSelected", "3", 30);
+};
+
+function setCookieNami() {
+  setCookie("cookieValueForCaseSelected", "4", 30);
+};
+
+function setCookieMaya() {
+  setCookie("cookieValueForCaseSelected", "5", 30);
+};
+
+function setCookieTrystan() {
+  setCookie("cookieValueForCaseSelected", "6", 30);
+};
+
+// Set `cookieValueForCaseSelected` to '2' if it doesn't exist
+const setCookieIfNotExists = (name, value, days) => {
+  if (!getCookie(name)) {
+      let expires = "";
+      if (days) {
+          const date = new Date();
+          date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+          expires = ";expires=" + date.toUTCString();
+      }
+      document.cookie = `${name}=${value}${expires};path=/;samesite=lax`;
+  }
+};
+
+const getCookie = (name) => {
+  const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+  return match ? match[2] : null;
+};
+
+setCookieIfNotExists("cookieValueForCaseSelected", "2", 30);
+
+// Cookie for remembering which form, change link, was pressed on
+const changeLinkClickedOnForm = document.getElementsByClassName("change");
+
+Array.from(changeLinkClickedOnForm).forEach(link =>{ 
+    if (document.referrer.includes("new")) {
+      link.addEventListener("click", setCookieFormNew);
+     } else if (document.referrer.includes("accepted")){
+      link.addEventListener("click", setCookieFormAccepted);
+    } else {
+      link.addEventListener("click", setCookieFormClosed);
+    }
+  }
+);
+
+function setCookieFormNew() {
+  setCookie("theFormChangeLinkClicked", "New", 30);
+}
+
+function setCookieFormAccepted() {
+  setCookie("theFormChangeLinkClicked", "Accepted", 30);
+}
+
+function setCookieFormClosed() {
+  setCookie("theFormChangeLinkClicked", "Closed", 30);
+}
+
